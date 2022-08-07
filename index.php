9d59b8cfcd0427906a6a844d2d1d96036ed80c00
goutam-kumar1 <107395727+goutam-kumar1@users.noreply.github.com>
2022-08-07 15:58:46 +0500

    my class work

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Foam</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <h2>  Student Data   </h2>
+        <label for="name"> Name:  </label>
+        <input type="name"  name="name"  placeholder="Your Name" required> <br><br>
+
+        <label for="name"> Email:  </label>
+        <input type="name"  name="name"  placeholder="Your Email" required><br><br>
+
+
+        <label for="name"> Pasword:  </label>
+        <input type="name"  name="name"  placeholder="Your Pasword" required>
+
+        <input type="Submit" value="Submit" name= "Submit">
+
+
+
+
+
+
+
+
+
+    </form>
+</body>
+</html>
